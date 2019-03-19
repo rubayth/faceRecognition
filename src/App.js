26e71e286bcdd681b17012c3198e6642042f1666
Rubayth Haque <rubayth19@gmail.com>
2019-03-19 15:59:22 -0400

    added functionality, face detiontion bug

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition.js';
 import Rank from './components/Rank/Rank';
 import Footer from './components/Footer/Footer';
 import './App.css';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+
+
+const app = new Clarifai.App({
+  apiKey: '7350eb9ef1ff4881ad73d5cc496c6645'
+ });
 
 const particlesOptions={
   particles: {
     number: {
-      value: 30,
+      value: 100,
       density: {
         enable: true,
         value_area:800
@@ -19,6 +26,49 @@ const particlesOptions={
   }
 }
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      input:'',
+      imageUrl:'',
+      box:{},
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    console.log(data);
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height),
+    }
+  }
+
+  displayFace = (box) => {
+    this.setState({box: box});
+  }
+
+
+  onInputChange = (event) =>{
+    this.setState({input: event.target.value})
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl:this.state.input})
+
+    app.models.predict(
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input)
+      .then(response => this.displayFace(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <div className="App">
@@ -26,9 +76,12 @@ class App extends Component {
         <Navigation/>
         <Logo/>
         <Rank/>
-        <ImageLinkForm/>
-        {//<FaceRecognition/>}
-        }
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit} />
+        <FaceRecognition 
+          imageUrl={this.state.imageUrl}
+          box={this.state.box}/>
         <Footer/>
       </div>
     );
