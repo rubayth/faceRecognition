3def8f9cda478a7d52ebf729279c89f902abda77
Rubayth Haque <rubayth19@gmail.com>
2019-04-18 12:56:56 -0400

    Updates

---
+++ precache-manifest.a9ac7f7f928dd9f97ff8266785f849a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dab1a08a95ef64a151c4",
+    "url": "/faceRecognition/static/js/runtime~main.646336b4.js"
+  },
+  {
+    "revision": "73dbcbadb16b31a814f9",
+    "url": "/faceRecognition/static/js/main.fd1f2205.chunk.js"
+  },
+  {
+    "revision": "38338836d04a28f85f79",
+    "url": "/faceRecognition/static/js/2.b47773db.chunk.js"
+  },
+  {
+    "revision": "73dbcbadb16b31a814f9",
+    "url": "/faceRecognition/static/css/main.ce8314e6.chunk.css"
+  },
+  {
+    "revision": "38338836d04a28f85f79",
+    "url": "/faceRecognition/static/css/2.992aa394.chunk.css"
+  },
+  {
+    "revision": "88e138228594d4419fb4bef42e4a5cb0",
+    "url": "/faceRecognition/index.html"
+  }
+];
